ff2151e24f9cd75cbf1c2a94f613209e23b35aac
Anisa Muziya Rafa <amrxll08@gmail.com>
2025-03-14 16:51:09 +0800

    Update Tugas_Simulasi.py

--- Tugas_Simulasi.py
+++ Tugas_Simulasi.py
@@ -10,16 +10,16 @@ for list in matriks_list:
 
 print('-'*70)
 print("Elemen baris pertama kolom pertama dari matriks tersebut adalah : ",matriks_list[0][0])
-print("Elemen baris kedua   kolom pertama dari matriks tersebut adalah :",matriks_list[1][0])
-print("Elemen baris keriga  kolom pertama dari matriks tersebut adalah :",matriks_list[2][0])
+print("Elemen baris kedua   kolom pertama dari matriks tersebut adalah :", matriks_list[1][0])
+print("Elemen baris keriga  kolom pertama dari matriks tersebut adalah :", matriks_list[2][0])
 print('-'*70)
 print("Elemen baris pertama kolom kedua   dari matriks tersebut adalah : ",matriks_list[0][1])
-print("Elemen baris kedua   kolom kedua   dari matriks tersebut adalah :",matriks_list[1][1])
+print("Elemen baris kedua   kolom kedua   dari matriks tersebut adalah :", matriks_list[1][1])
 print("Elemen baris ketiga  kolom kedua   dari matriks tersebut adalah : ",matriks_list[2][1])
 print('-'*70)
 print("Elemen baris kedua   kolom ketiga  dari matriks tersebut adalah : ",matriks_list[1][2])
 print("Elemen baris ketiga  kolom ketiga  dari matriks tersebut adalah : ",matriks_list[2][2])
-print("Elemen baris pertama kolom ketiga  dari matriks tersebut adalah :",matriks_list[0][2])
+print("Elemen baris pertama kolom ketiga  dari matriks tersebut adalah :", matriks_list[0][2])
 print('-'*70)
 
 import numpy as np
@@ -30,17 +30,17 @@ matriks_array = ([[2, 1, -1],
 print("ini adalah matriks array")
 for array in matriks_array:
     print(array)
-    
+
 print('-'*70)
 print("Elemen baris pertama kolom pertama dari matriks tersebut adalah : ",matriks_array[0][0])
-print("Elemen baris kedua   kolom pertama dari matriks tersebut adalah :",matriks_array[1][0])
-print("Elemen baris keriga  kolom pertama dari matriks tersebut adalah :",matriks_array[2][0])
+print("Elemen baris kedua   kolom pertama dari matriks tersebut adalah :", matriks_array[1][0])
+print("Elemen baris keriga  kolom pertama dari matriks tersebut adalah :", matriks_array[2][0])
 print('-'*70)
 print("Elemen baris pertama kolom kedua   dari matriks tersebut adalah : ",matriks_array[0][1])
-print("Elemen baris kedua   kolom kedua   dari matriks tersebut adalah :",matriks_array[1][1])
+print("Elemen baris kedua   kolom kedua   dari matriks tersebut adalah :", matriks_array[1][1])
 print("Elemen baris ketiga  kolom kedua   dari matriks tersebut adalah : ",matriks_array[2][1])
 print('-'*70)
 print("Elemen baris kedua   kolom ketiga  dari matriks tersebut adalah : ",matriks_array[1][2])
 print("Elemen baris ketiga  kolom ketiga  dari matriks tersebut adalah : ",matriks_array[2][2])
-print("Elemen baris pertama kolom ketiga  dari matriks tersebut adalah :",matriks_array[0][2])
+print("Elemen baris pertama kolom ketiga  dari matriks tersebut adalah :", matriks_array[0][2])
 print('-'*70)
